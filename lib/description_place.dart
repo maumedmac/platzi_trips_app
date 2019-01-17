0bebc4812f00affb3963d54b5fc9a985e59263c9
Mauricio Medrano Machicado <maumedmac@gmail.com>
2019-01-17 16:36:02 -0400

    Modificacion del Texto

--- lib/description_place.dart
+++ lib/description_place.dart
@@ -22,9 +22,9 @@ class DescriotionPlace extends StatelessWidget{
         left: 20.0,
       ),
       child: Text(
-        "Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de las industrias desde el año 1500, cuando un impresor (N. del T. persona que se dedica a la imprenta) desconocido usó una galería de textos y los mezcló de tal manera que logró hacer un libro de textos especimen.",
+        "Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto.\n\nc Lorem Ipsum ha sido el texto de relleno estándar de las industrias desde el año 1500, cuando un impresor (N. del T. persona que se dedica a la imprenta) desconocido usó una galería de textos y los mezcló de tal manera que logró hacer un libro de textos especimen.",
         style: TextStyle(
-          fontSize: 12.0,
+          fontSize: 14.0,
         ),
         textAlign: TextAlign.left,
       ),
